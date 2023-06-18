c18b7492480370a8c970442ee2701f1848e2ddd9
Scott H. Hawley <scott.hawley@belmont.edu>
2023-06-17 22:50:54 -0500

    added OFC GUI to example/run.py

--- examples/run.py
+++ examples/run.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
-from prefigure.prefigure import get_all_args, push_wandb_config
+from prefigure import get_all_args, push_wandb_config, OFC
 
 import pytorch_lightning as pl
 import wandb
 import gin
-
+import time
 
 # Usage: ./run.py --name test-prefigure
 #  ^ the "test-prefigure" is the name of the wandb run
@@ -24,6 +24,7 @@ def main():
     #   3. Any new command-line arguments override whatever was set earlier
     args = get_all_args()
 
+
     test_print()
 
     print("Args = \n",args)
@@ -34,6 +35,14 @@ def main():
     print(f"Pushing args to wandb run {args.name}")
     wandb_logger = pl.loggers.WandbLogger(project=args.name)
     push_wandb_config(wandb_logger, args, omit=['training_dir']) # push config to wandb for use later
+
+    ofc = OFC(args, gui=True) 
+    print(f"OFC: Public link is {ofc.public_link}")
+    wandb.log({"gradio_link": wandb.Html(f'OFC Gradio URL is <a href="{ofc.public_link}" target="_blank">{ofc.public_link}</a>')})
+
+    while True:
+        changed = ofc.update()
+        time.sleep(2)
 
 if __name__ == '__main__':
     main()
